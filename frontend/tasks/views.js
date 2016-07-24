8a75d566dcbcb1795656eb946ae6ee477e4877dc
khlevnov <olegkhlevnov@gmail.com>
2016-07-25 01:33:10 +0300

    Format views gulp task code

--- frontend/tasks/views.js
+++ frontend/tasks/views.js
@@ -1,18 +1,15 @@
-'use strict';
-
 const gulp = require('gulp'),
-    notify = require('gulp-notify'),
     pug = require('gulp-pug');
 
 gulp.task('views', function() {
-	return gulp.src('frontend/views/**/*.pug')
+    return gulp.src('frontend/views/**/*.pug')
         .pipe(pug({
             pretty: '    ',
         }))
-		.pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('views:production', function() {
-	return gulp.src('frontend/views/**/*.pug')
-		.pipe(gulp.dest('app/Resources/views'));
+    return gulp.src('frontend/views/**/*.pug')
+        .pipe(gulp.dest('app/Resources/views'));
 });
