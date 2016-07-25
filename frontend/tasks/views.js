43cb828f6230d89432a474f5fd07331e16d1f334
khlevnov <olegkhlevnov@gmail.com>
2016-07-25 22:53:20 +0300

    Добавил фейковый джейсон для статей

--- frontend/tasks/views.js
+++ frontend/tasks/views.js
@@ -2,14 +2,17 @@ const gulp = require('gulp'),
     pug = require('gulp-pug');
 
 gulp.task('views', function() {
+    var fakedata = require(__dirname + '/../json/fakedata.json');
+
     return gulp.src('frontend/views/**/*.pug')
         .pipe(pug({
             pretty: '    ',
+            locals: fakedata
         }))
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('views:production', function() {
-    return gulp.src('frontend/views/**/*.pug')
-        .pipe(gulp.dest('app/Resources/views'));
+    return gulp.src('frontend/**/*.pug')
+        .pipe(gulp.dest('app/Resources'));
 });
