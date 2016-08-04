d2a79771b11ee32e09f6ae4507efde117cebc2ad
khlevnov <olegkhlevnov@gmail.com>
2016-08-05 00:35:20 +0300

    Добавил переходы на экраны на главной

--- frontend/blocks/menu/menu.js
+++ frontend/blocks/menu/menu.js
@@ -3,10 +3,16 @@ import throttle from 'lodash/throttle';
 let menu = document.getElementById('menu'),
     headerHeight = window.innerHeight - menu.clientHeight;
 
-window.addEventListener('scroll', throttle(function() {
+window.addEventListener('scroll', throttle(() => {
     if (window.pageYOffset > headerHeight) {
         menu.classList.add('menu--fixed');
     } else {
         menu.classList.remove('menu--fixed');
     }
 }, 50));
+
+Array.from(menu.querySelectorAll('.menu__item')).forEach((button) => {
+    button.addEventListener('click', () => {
+        document.getElementById(button.dataset.scroll).scrollIntoView();
+    });
+})
